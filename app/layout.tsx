823e845c4f23f8e711ddbd908d96df8d91b1d951
Joziberto <jozibertoalvesteixeirafilho@gmail.com>
2026-05-17 18:12:50 -0300

    hero ready and modifications on font to poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+
+const poppins = Poppins({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -24,10 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="pt-BR"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`${poppins.className} h-full antialiased min-h-full flex flex-col`}>{children}</body>
     </html>
   );
 }
